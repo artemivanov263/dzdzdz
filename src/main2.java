29efea04ae72a58c327a9aad057af07a81db04f6
artem <artemiyivanov134@gmail.com>
2024-10-16 15:24:09 +0500

    commit dz.06

--- src/main2.java
+++ src/main2.java
@@ -4,19 +4,32 @@ import static method.Method.*;
 public class main2 {
 
     public static void main(String[] args) {
+
         int x = 5;
         int y = square(5);
         System.out.println(y);
 
         String name = "bob";
         String fuullname = "ibanelevilo";
-        System.out.println(printFullName(name, fuullname));
+        System.out.println(printFullName(name,fuullname));
 
 
         String[] macciv = {"da", "dqwdqwdqwdq", "dqwdqwdqwdqwqqqqqqqq", "dqdqwdqwdq"};
         String[] vsvod = corneyValuesArray(macciv);
         System.out.println("max : " + vsvod[0]);
         System.out.println("min : " + vsvod[1]);
+
+
+        int[] arrays = new int[]{5, 56, 5453, 543, 11113, 3123123,};
+        int[] maxmin = corneyValuesArray(arrays);
+        System.out.println("max : " + maxmin[0]);
+        System.out.println("min : " + maxmin[1]);
+
+
+        char [] maxminchar = new char[] { 's','f','z','s','i'};
+        char [] resultmaxminchar = corneyValuesArray(maxminchar);
+        System.out.println("maxminchar : " + resultmaxminchar[0]);
+        System.out.println("minminchar : " + resultmaxminchar[1]);
 
     }
 }
--- src/method/Method.java
+++ src/method/Method.java
@@ -1,37 +1,71 @@
 package method;
 
-import java.util.Arrays;
-
 public class Method {
 
-    public static void main(String[] args) {
-
-    }
-
+    // принимает значение инта
     public static int square(int a) {
 
-        return a * a;
+        return a * a;  // возврат умноженного само на себя значения
     }
 
-    public static String printFullName(String a, String b) {
-        String fullname = a + " " + b;
-        return fullname;
+    public static String printFullName(String... a) {// принимает несколько строк
+        String [] strings = new String[a.length];
+        strings[0] = a[0];
+        strings[1] = a[1];
+        String fullname = strings[0] + " " + strings[1]; // сложение строк в одну
+        return fullname;  // ретерн плонго имя фамилии
 
     }
 
-    public static String[] corneyValuesArray(String[] a) {
-        String max = a[0];
-        String min = a[0];
-        for (int i = 0; i < a.length; i++) {
-            if (a[i].length() > max.length()) {
-                max = a[i];
-            } else if (a[i].length() < min.length()) {
-                min = a[i];
+    public static String[] corneyValuesArray(String... strings) { // принял стринг вернет массив стринга
+        String max = strings[0];
+        String min = strings[0];
+        for (int i = 0; i < strings.length; i++) { // провекра циклом макс и мин значения
+            if (strings[i].length() > max.length()) {
+                max = strings[i];
+            } else if (strings[i].length() < min.length()) {
+                min = strings[i];
             }
         }
-        String[] maxmin = new String[2];
+        String[] maxmin = new String[2]; // массив из вдух строк
+        maxmin[0] = max; // присваевате мах
+        maxmin[1] = min; // присваевает мин
+        return maxmin; // ретерн массива
+    }
+
+   public static int [] corneyValuesArray(int... a) { // принял  инт вернет массив инт
+        int max = a[0];
+        int min = a[0];
+        for (int i = 0; i < a.length; i++) {  // провекра циклом макс и мин значения
+            if (a[i] > max) {
+                max = a[i];
+
+            } else if (a[i] < min) {
+                min = a[i];
+
+            }
+        }
+        int[] massivmaxmin = new int[2];
+        massivmaxmin[0] = max;
+        massivmaxmin[1] = min;
+
+       return massivmaxmin; // ретерн масиива интов
+   }
+    public static char [] corneyValuesArray(char... a){
+        char max = a[0];
+        char min = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }else if (a[i] < min) {
+                min = a[i];
+            }
+        }char [] maxmin = new char[2];
         maxmin[0] = max;
         maxmin[1] = min;
+
+
         return maxmin;
     }
 }
+
